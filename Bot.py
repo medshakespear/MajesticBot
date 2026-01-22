7820e57c59b7a4a555c73f520675c03805d0d909
medshakespear <137207462+medshakespear@users.noreply.github.com>
2026-01-22 06:50:33 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -468,6 +468,7 @@ async def squad_info(interaction: discord.Interaction):
 
 # -------------------- INSTANT ROLE SYNC --------------------
 @bot.event
+await bot.wait_until_ready()
 async def on_member_update(before, after):
     role, tag = get_member_squad(after, after.guild)
     await safe_nick_update(after, role, tag)
